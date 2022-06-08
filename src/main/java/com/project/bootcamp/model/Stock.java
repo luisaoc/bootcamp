eb7fcd746fef2ff68613a50a7b7d800518d90d88
Luis Carlos de Oliveira Zeferino <luisaoc@gmail.com>
2022-06-08 13:32:04 -0300

    criação de repositório dto bootcamp

---
+++ src/main/java/com/project/bootcamp/model/Stock.java
@@ -0,0 +1,61 @@
+package com.project.bootcamp.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_stock")
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "date")
+    private LocalDate date;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "price")
+    private Double price;
+    @Column(name = "variation")
+    private Double variation;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getVariation() {
+        return variation;
+    }
+
+    public void setVariation(Double variation) {
+        this.variation = variation;
+    }
+}
